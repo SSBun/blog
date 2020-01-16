f4259fee96bee52922fa4ddce9a97267268a70c8
蔡士林 <caishilin@babyfs.cn>
2020-01-16 15:41:56 +0800

    Update posts

--- update_post.sh
+++ update_post.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if test $# -ge 1; then
+    if test $1 -eq '-a'; then
+        cd themes/cactus
+        source update.sh
+ 	cd ...
+    fi 
+fi
+
 git add -A
 git commit -m "Update posts"
 git push origin master
